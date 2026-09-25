6f9cc88a4acf6ec67d8af69c211880c3cbd6c5a2
Rahul Mahato <rahulm4080@gmail.com>
2026-09-26 04:00:41 +0530

    Fix production glass blur

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,10 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
+  plugins: [react(), tailwindcss()],
+  build: {
+    cssMinify: "esbuild",
   },
 });
